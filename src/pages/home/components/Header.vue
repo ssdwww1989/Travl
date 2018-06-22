61522f7329996c628d005f4c253c54fa78f60751
chenliang-home <ssdwww1989@gmail.com>
2018-06-22 21:37:45 +0800

    add header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="header">
-        <div class="header-left">返回</div>
-        <div class="header-input">内容</div>
-        <div class="header-right">城市</div>
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe647;</div>
+        </div>
+        <div class="header-input">
+             <span class="iconfont">&#xe60b;</span>
+            输入城市/景点/游玩主题
+        </div>
+        <div class="header-right">
+            城市
+             <span class="iconfont arrow-icon">&#xe6aa;</span>
+        </div>
     </div>
 </template>
 
@@ -14,15 +22,36 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
- .header
-    display: flex
-    height： .86rem
-    .header-left
-      float: left
-    .header-input
-      flex: 1
-    .header-right
-      float: right
+    @import '~styles/varibles.styl'
+    .header
+      height .86rem
+      display flex
+      color #fff
+      line-height .86rem
+      background $bgColor
+      .header-left
+        width .64rem
+        float left
+        .back-icon
+          text-align center
+          font-size .4rem
+      .header-input
+        flex 1
+        line-height .64rem
+        padding-left .2rem
+        height .64rem
+        margin-top .12rem
+        margin-left .2rem
+        background #fff
+        border-radius 5px
+        color #ccc
+      .header-right
+        width 1.24rem
+        float right
+        text-align center
+        .arrow-icon
+          margin-left -.04rem
+          font-size .24rem
 </style>
 
 
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <homeHeader></homeHeader>
+        <home-header></home-header>
     </div>
 </template>
 
