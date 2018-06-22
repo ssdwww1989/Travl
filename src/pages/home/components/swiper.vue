eb77f9d37f22ca79845bb07f7b0c202950e2b87e
chenliang-home <ssdwww1989@gmail.com>
2018-06-22 23:13:26 +0800

    提交index-icons 分支

--- src/pages/home/components/swiper.vue
+++ src/pages/home/components/swiper.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="wrapper">
     <swiper :options="swiperOption">
+      
         <swiper-slide v-for="item of swiperList" :key="item.id">
             <img class="swiper-img" :src="item.imgUrl" alt="">
         </swiper-slide>
