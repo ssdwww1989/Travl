f3f5a7d7731b32319a24e9ea8fae22c994f01bd2
chenliang-home <ssdwww1989@gmail.com>
2018-06-23 00:22:44 +0800

    增加homeicon

--- src/pages/home/components/swiper.vue
+++ src/pages/home/components/swiper.vue
@@ -1,7 +1,6 @@
 <template>
 <div class="wrapper">
     <swiper :options="swiperOption">
-      
         <swiper-slide v-for="item of swiperList" :key="item.id">
             <img class="swiper-img" :src="item.imgUrl" alt="">
         </swiper-slide>
