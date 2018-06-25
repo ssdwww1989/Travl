d9e6c1c8ada7f4e3cf95c00c3fff2a02eb9f6d72
chen_appleseed <chen_appleseed@bogon.home>
2018-06-25 18:52:14 +0800

    header 动画开发完成

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,17 +1,25 @@
 <template>
- <detail-banner></detail-banner>
+ <div>
+   <detail-header></detail-header>
+   <detail-banner></detail-banner>
+   <div class="content"></div>
+ </div>
 </template>
 
 <script>
 import detailBanner from './components/banner'
+import detailHeader from './components/header'
 export default {
   components: {
-    detailBanner
+    detailBanner,
+    detailHeader
+
   },
   name: 'Detail'
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .content
+    height 50rem
 </style>
