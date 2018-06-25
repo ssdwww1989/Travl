862bf0eba881f715e1c1efbf4e675e2a104f5f03
chen_appleseed <chen_appleseed@bogon.home>
2018-06-25 10:02:14 +0800

    提升代码性能

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -23,8 +23,12 @@ export default {
   },
   data () {
     return {
-      touchStaus: false
+      touchStaus: false,
+      startY: 0
     }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
   },
   methods: {
     handleLetterClick (e) {
@@ -35,9 +39,8 @@ export default {
     },
     handleTouchMove (e) {
       if (this.touchStaus) {
-        const startY = this.$refs['A'][0].offsetTop
         const touchY = e.touches[0].clientY - 89
-        const index = Math.floor((touchY - startY) / 20)
+        const index = Math.floor((touchY - this.startY) / 20)
         if (index >= 0 && index < this.letters.length) {
           this.$emit('change', this.letters[index])
         }
